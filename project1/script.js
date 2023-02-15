5bb84fe9cff2175b27b0022ef22c5eff48086c53
Alaa Al Shaba <ama10003@nyu.edu>
2023-02-15 17:47:31 +0400

    updated

--- project1/script.js
+++ project1/script.js
@@ -1,31 +1,24 @@
-
-//multiple slideshows
-let slideIndex =[1,1];
+let slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
 let slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
 showSlides(1, 1);
 
+
 // Next/previous controls
 function plusSlides(n, no) {
-    showSlides(slideIndex[no] += n, no);
+  showSlides(slideIndex[no] += n, no);
 }
-
-//for the dots to know which is the current slide
-function currentSlide(n,no) {
-    showSlides(slideIndex = n,no);
-  }
 
 
   //displaay each slide depending on which slideshow we are clikcing on
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
+  if (n > x.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-    // dots[i].className = dots[i].className.replace(" active", "");
+    x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  x[slideIndex[no]-1].style.display = "block";
 }

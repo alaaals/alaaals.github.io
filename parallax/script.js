d8bc7bd8d9fd50d366b3149cc04179abcb5d08a6
Alaa Al Shaba <ama10003@nyu.edu>
2023-03-05 23:30:06 +0400

    scroll website

---
+++ parallax/script.js
@@ -0,0 +1,27 @@
+$("#1st").click (() => {
+	$("#1st").text("You have clicked on the first box!")
+})
+
+$("#2nd").click (() => {
+	$("#2nd").text("You have clicked on the second box!")
+})
+
+$("#3rd").click (() => {
+	$("#3rd").text("You have clicked on the third box!")
+})
+
+$("#4th").click (() => {
+	$("#4th").text("You have clicked on the fourth box!")
+})
+
+$("#5th").click (() => {
+	$("#5th").text("You have clicked on the fifth box!")
+})
+
+$("#6th").click (() => {
+	$("#6th").text("You have clicked on the sixth box!")
+})
+
+
+ AOS.init()
+ 
